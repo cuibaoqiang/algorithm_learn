3eeabe6000bb4c1ec2687621797dac6fdd40bcfe
cuibaoqiang <826111126@qq.com>
2021-07-15 10:18:35 +0800

    汉诺塔游戏

--- src/main/java/com/algorithm/learn/Hanoi.java
+++ src/main/java/com/algorithm/learn/Hanoi.java
@@ -15,10 +15,14 @@ public class Hanoi {
      */
     private static void hanoi(int n, char origin, char assist, char destination) {
         if (n == 1) {
+            // 输出移动过程
             System.out.println("Direction:" + origin + "--->" + destination);
         } else {
+            // 将 n-1 个盘子从源座借助目的座移动到辅助座上面
             hanoi(n - 1, origin, destination, assist);
+            // 将剩余的 1 个盘子从源座借助辅助座移动到目的座上面
             hanoi(1, origin, assist, destination);
+            // 将 n-1 个盘子从辅助座借助源座移动到目的座上面
             hanoi(n - 1, assist, origin, destination);
         }
     }
