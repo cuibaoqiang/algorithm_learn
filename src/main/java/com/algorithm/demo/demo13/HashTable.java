698f5521ccd4ff40f457f6da6087f6911d7650a7
cuibaoqiang <826111126@qq.com>
2022-02-26 13:34:10 +0800

    哈希表

---
+++ src/main/java/com/algorithm/demo/demo13/HashTable.java
@@ -0,0 +1,33 @@
+package com.algorithm.demo.demo13;
+
+import java.util.Arrays;
+
+/**
+ * @author cuibaoqiang
+ * @date 2022-02-26 13:32:56
+ * @desc
+ */
+public class HashTable {
+    private StuInfo[] data = new StuInfo[100];
+
+    /**
+     * 向散列表中添加元素
+     *
+     * @param stuInfo
+     */
+    public void put(StuInfo stuInfo) {
+        //调用散列函数获取存储位置
+        int index = stuInfo.hashCode();
+        //添加元素
+        data[index] = stuInfo;
+    }
+
+    public StuInfo get(StuInfo stuInfo) {
+        return data[stuInfo.hashCode()];
+    }
+
+    @Override
+    public String toString() {
+        return "HashTable [data=" + Arrays.toString(data) + "]";
+    }
+}
