55f8a5f48c1d23893368ccffc51cf5b649deb0d1
cuibaoqiang <826111126@qq.com>
2022-02-28 10:18:10 +0800

    LRU

--- src/main/java/com/algorithm/leetcode/LeetCode0146/LRUCache.java
+++ src/main/java/com/algorithm/leetcode/LeetCode0146/LRUCache.java
@@ -10,6 +10,25 @@ import java.util.Map;
  */
 public class LRUCache {
 
+    public static void main(String[] args) {
+        //["LRUCache","put","put","get","put","get","put","get","get","get"]
+        //[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        System.out.println(cache.cache);
+        cache.put(2, 2);
+        System.out.println(cache.cache);
+        System.out.println(cache.get(1));
+        cache.put(3, 3);
+        System.out.println(cache.cache);
+        System.out.println(cache.get(2));
+        cache.put(4, 4);
+        System.out.println(cache.cache);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
+    }
+
     class DLinkedNode {
         int key;
         int value;
@@ -22,6 +41,11 @@ public class LRUCache {
         public DLinkedNode(int key, int value) {
             this.key = key;
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value + "";
         }
     }
 
