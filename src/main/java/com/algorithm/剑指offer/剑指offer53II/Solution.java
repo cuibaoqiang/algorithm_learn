2427b0f675924f2f77b1107b1ae4c9179243c71e
cuibaoqiang <826111126@qq.com>
2022-08-09 09:52:31 +0800

    0 - n-1中缺少的数字

--- src/main/java/com/algorithm/剑指offer/剑指offer53II/Solution.java
+++ src/main/java/com/algorithm/剑指offer/剑指offer53II/Solution.java
@@ -3,7 +3,7 @@ package com.algorithm.剑指offer.剑指offer53II;
 /**
  * @author cuibaoqiang
  * @date 2022-08-08 16:22:40
- * @desc
+ * @desc 0 - n-1中缺少的数字
  */
 public class Solution {
 
@@ -14,7 +14,11 @@ public class Solution {
     }
 
     public int missingNumber(int[] nums) {
-
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return nums.length * (1 + nums.length) / 2 - sum;
     }
 }
 
