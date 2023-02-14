464df7048bd5b613c2dc2a9bf36c4d6361762e1a
cuibaoqiang <826111126@qq.com>
2023-02-14 08:55:19 +0800

    nowcode

---
+++ src/main/java/com/algorithm/nowcoder/BM6/Solution.java
@@ -0,0 +1,55 @@
+package com.algorithm.nowcoder.BM6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author cuibaoqiang
+ * @date 2023-02-14 08:36:49
+ * @desc BM6 判断链表中是否有环
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution code = new Solution();
+        ListNode l1 = new ListNode(1, new ListNode(3, new ListNode(9, new ListNode(9, new ListNode(9)))));
+
+        ListNode l2 = new ListNode(1, new ListNode(2, new ListNode(9, new ListNode(9))));
+
+        ListNode node = code.mergeKLists(new ArrayList<>(Arrays.asList(l1, l2)));
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow.equals(fast)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
